898711750efae2dcf33c44faf019610f9f49264a
Firdaus Ahmed Siddiqui <64842162+FIRDAUS77@users.noreply.github.com>
2020-12-18 21:52:18 +0530

    Add files via upload

---
+++ Banking-System-main/README.md
@@ -0,0 +1,11 @@
+# Banking-System
+
+My internship task from The spark foundation(TSF)-GRIP for WebDevelopment project. Completed the project using HTML, CSS, Bootstrap, JavaScript and PHP database used MySQL.
+
+Hosting platform -> 000Webhost.
+
+Website link :
+
+Developed by: Firdaus Ahmed Siddiqui
+
+Contact: firdausahmed47@gmail.com
---
+++ Banking-System-main/index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Siddiqui's Banking System</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <style type="text/css">
+    .logo-text{
+	  font-size:50px;
+	  padding-left: 40%;
+      color: white;
+      padding-top: 20px;
+	  
+    }
+    nav{
+      background-image:url("bank.jfif");
+      background-color:grey;
+      background-blend-mode:overlay;
+    }
+    
+    .main-img{
+      width:100%;
+      margin: 0;
+    }
+    .main-text{
+      text-align: center;
+      font-size: 40px;
+    }
+    .p2{
+      font-size: 30px;
+    }
+    body{
+      background-color:rgb(94,55,75);
+    }
+    .button1 {
+      background-color: #e85d04;
+      color: white;
+      padding:15px  32px;
+      text-align: center;
+      text-decoration: none;
+      
+      font-size: 20px;
+      margin: 0px 2px;
+      border-radius: 60px;
+    }
+	.button2 {
+      background-color: #e85d04;
+      border: none;
+      color: white;
+      padding: 15px 32px;
+      text-align: center;
+      text-decoration: none;
+     
+      font-size: 20px;
+      margin: 0px 10px;
+      border-radius: 60px;
+    }
+    
+    .button1:hover{
+      background-color: purple;
+      text-decoration: none;
+      color: #0b090a;
+    }
+    .button2:hover{
+      background-color: purple;
+      text-decoration: none;
+      color: #0b090a;
+    }
+      
+    
+	.view-btn2{
+      text-align:center ;
+    }
+	.mo{
+		 background-color: grey;
+		  padding: 50px 50px;
+		   margin: 10% 20%;
+	}
+  .footer1{
+    background-color:grey;
+    height:76px;
+  }
+  .footer p{
+    font-family: sans-serif;
+    color:white;
+  }
+
+    </style>
+
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+ 
+  <p class="logo-text">Siddiqui's Bank</p>
+  </nav>
+  
+ <div class="mo" >
+ <div class="view-btn2">
+     
+  </div>
+  <br></br>
+  <div class="view-btn2">
+     <a href="viewusers.php" class="button1">Customer Details</a>
+  </div>
+  <br></br>
+   
+  <div class="view-btn2">
+     <a href="transactionDetails.php" class="button2">Transaction History</a>
+  </div>
+
+
+  </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  
+  
+  
+   <footer class="footer1" align="center"> <br>
+    <div  style="height: 60%">
+    <p>All Copyrights Reseved | &copy; Siddiqui's Bank | Contact +91 7653886489</p>
+ 
+    </div>
+  </footer>
+  
+  
+  
+  </body>
+</html>
